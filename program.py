884729376470d000755be3c3ca377b9e34e58410
Harisaya <vugiaken@gmail.com>
2025-03-06 23:24:28 +0700

    tuangikobt-2

--- program.py
+++ program.py
@@ -3,45 +3,45 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from PyQt6 import uic
+import sys
 from database import *
 
 class MessageBox():
-    def succes_box(self, message): 
+    def success_box(self,message):
         box = QMessageBox()
         box.setWindowTitle("Success")
         box.setText(message)
         box.setIcon(QMessageBox.Icon.Information)
         box.exec()
-    
-    def error_box(self, message):
+    def error_box(self,message):
         box = QMessageBox()
-        box.setWindowTitle ("Error")
+        box.setWindowTitle("Error")
         box.setText(message)
         box.setIcon(QMessageBox.Icon.Critical)
         box.exec()
-
+    
 class Login(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(Login,self).__init__()
         uic.loadUi("ui/login.ui",self)
-        
-        self.email = self.findChild(QLineEdit, "txt_email")
-        self.password = self.findChild(QLineEdit, "txt_password")
-        self.btn_login = self.findChild(QPushButton, "btn_login")
-        self.btn_register = self.findChild(QPushButton, "btn_register")
-        self.btn_eye_p = self.findChild(QPushButton, "btn_eye_p")
 
-        self.btn_login.clicked.connect(self.login)
+        self.email = self.findChild(QLineEdit,"txt_email")
+        self.password = self.findChild(QLineEdit,"txt_password")
+        self.btn_login = self.findChild(QPushButton,"btn_login")
+        self.btn_register = self.findChild(QPushButton,"btn_register")
+        self.btn_eye_p = self.findChild(QPushButton,"btn_eye_p")
+
+        self.btn_login.clicked.connect(self.register)
         self.btn_register.clicked.connect(self.show_register)
-        self.btn_eye_p.clicked.connect(lambda: self.hiddenOrShow(self.password, self.btn_eye_p))
+        self.btn_eye_p.clicked.connect(lambda : self.hiddenOrShow(self.password,self.btn_eye_p))
 
-    def hiddenOrShow(self, input:QLineEdit, button:QPushButton):
+    def hiddenOrShow(self,input:QLineEdit,btn:QPushButton):
         if input.echoMode() == QLineEdit.EchoMode.Password:
             input.setEchoMode(QLineEdit.EchoMode.Normal)
-            button.setIcon(QIcon("img/eye-solid.svg"))
+            btn.setIcon(QIcon("img/eye-solid.svg"))
         else:
             input.setEchoMode(QLineEdit.EchoMode.Password)
-            button.setIcon(QIcon("img/eye-slash-solid.svg"))
+            btn.setIcon(QIcon("img/eye-slash-solid.svg"))
 
     def login(self):
         msg = MessageBox()
@@ -49,59 +49,65 @@ class Login(QMainWindow):
         password = self.password.text()
 
         if email == "":
-            msg.error_box("Email không được để trống")
+            msg.error_box("không thể để trống")
             self.email.setFocus()
             return
-        
+
         if password == "":
-            msg.error_box("Mật khẩu không được để trống")
+            msg.error_box("không thể để trống")
             self.password.setFocus()
             return
         
-        user = get_user_by_email_and_password(email, password)
-        if user is not None: 
-            msg.succes_box("Đăng nhập thành công")
+        user = get_user_by_email_and_password(email,password)
+        if user is not None:
+            msg.success_box("Đăng nhập thành công")
             self.show_home(user["id"])
             return
-        
-        msg.error_box("Email hoặc mật khẩu không đúng")
+
+        msg.error_box("Đăng nhập thất bại")
+
+    def show_home(self,user_id):
+        self.home = Home(user_id)
+        self.home.show()
+        self.close()
 
     def show_register(self):
         self.register = Register()
         self.register.show()
         self.close()
-        
-    def show_home(self, user_id):
-        self.home = Home(user_id)
-        self.home.show()
+    
+    def show_register(self):
+        self.register = Register()
+        self.register.show()
         self.close()
 
 class Register(QMainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi("ui/register.ui", self)
+        super(Register,self).__init__()
+        uic.loadUi("ui/resgin.ui",self)
 
-        self.name = self.findChild(QLineEdit, "txt_name")
-        self.email = self.findChild(QLineEdit, "txt_email")
-        self.password = self.findChild(QLineEdit, "txt_password")
-        self.confirm_password = self.findChild(QLineEdit, "txt_conf_pwd")
-        self.btn_register = self.findChild(QPushButton, "btn_register")
-        self.btn_login = self.findChild(QPushButton, "btn_login")
-        self.btn_eye_p = self.findChild(QPushButton, "btn_eye_p")
-        self.btn_eye_cp = self.findChild(QPushButton, "btn_eye_cp")
+        self.name = self.findChild(QLineEdit,"txt_name")
+        self.email = self.findChild(QLineEdit,"txt_email")
+        self.password = self.findChild(QLineEdit,"txt_password")
+        self.confirm_password = self.findChild(QLineEdit,"txt_conf_pwd")
+        self.btn_register = self.findChild(QPushButton,"btn_register")
+        self.btn_login = self.findChild(QPushButton,"btn_login")
+        self.btn_eye_p = self.findChild(QPushButton,"btn_eye_p")
+        self.btn_eye_cp = self.findChild(QPushButton,"btn_eye_cp")
 
-        self.btn_register.clicked.connect(self.register)
+        self.btn_register.clicked.connect(self.signup)
         self.btn_login.clicked.connect(self.show_login)
-        self.btn_eye_p.clicked.connect(self.hiddenOrShow(self.password, self.btn_eye_p))
-        self.btn_eye_cp.clicked.connect(self.hiddenOrShow(self.confirm_password, self.btn_eye_cp))
-    
-    def hiddenOrShow(self,input:QLineEdit, button: QPushButton):
+        self.btn_eye_p.clicked.connect(lambda : self.hiddenOrShow(self.password,self.btn_eye_p))
+        self.btn_eye_cp.clicked.connect(lambda : self.hiddenOrShow(self.confirm_password,self.btn_eye_cp))
+
+    def hiddenOrShow(self,input:QLineEdit,btn:QPushButton):
         if input.echoMode() == QLineEdit.EchoMode.Password:
             input.setEchoMode(QLineEdit.EchoMode.Normal)
-            button.setIcon(QIcon("img/eye-solid.svg"))
+            btn.setIcon(QIcon("img/eye-solid.svg"))
         else:
-            input.setEchoMode(QLineEdit.EchoMode.Normal)
-            button.setIcon(QIcon("img/eye-slash-solid.svg"))
+            input.setEchoMode(QLineEdit.EchoMode.Password)
+            btn.setIcon(QIcon("img/eye-slash-solid.svg"))
+
     def register(self):
         msg = MessageBox()
         name = self.name.text()
@@ -110,31 +116,30 @@ class Register(QMainWindow):
         confirm_password = self.confirm_password.text()
 
         if name == "":
-            msg.error_box("Họ tên không được để trống")
+            msg.error_box("Không thể để trống")
             self.name.setFocus()
             return
         
         if email == "":
-            msg.error_box("Email không được để trống")
+            msg.error_box("Không thể để trống")
             self.email.setFocus()
             return
         
         if password == "":
-            msg.error_box("Mật khẩu không được để trống")
+            msg.error_box("Không thể để trống")
             self.password.setFocus()
             return
         
         if confirm_password == "":
-            msg.error_box("Xác Nhận Mật khẩu không được để trống")
+            msg.error_box("Không thể để trống")
             self.confirm_password.setFocus()
             return
         
         if password != confirm_password:
             msg.error_box("Mật khẩu không trùng khớp")
-            self.confirm_password.setText("")
             self.password.setFocus()
             return
-    
+        
         if not self.validate_email(email):
             msg.error_box("Email không hợp lệ")
             self.email.setFocus()
@@ -143,33 +148,31 @@ class Register(QMainWindow):
         check_email = get_user_by_email(email)
         if check_email is not None:
             msg.error_box("Email đã tồn tại")
+
             return
         
         create_user(name,email,password)
-        msg.succes_box("Đăng ký thành công")
+        msg.success_box("Đăng ký thành công")
         self.show_login()
 
     def validate_email(self,s):
-        idx_at = s.find('@')
+        idx_at = s.find("@")
         if idx_at == -1:
             return False
-        return '-' in s [idx_at+1:]
-    
+        return '.' in s[idx_at+1:]
+
     def show_login(self):
         self.login = Login()
         self.login.show()
         self.close()
-
-class Home(QMainWindow):
-    def __init__(self, user_id):
-        super().__init__()
-        uic.loadUi("ui/home.ui", self)
-
-        self.user_id = user_id
         
+class Home(QMainWindow):
+    def __init__(self,user_id):
+        super(Home,self).__init__()
+        uic.loadUi("ui/menu.ui",self)
 
-if __name__ == "__main__":
-    app = QApplication([])
+if __name__ == "_main_":
+    app = QApplication(sys.argv)
     login = Login()
     login.show()
-    app.exec()
+    sys.exit(app.exec())
